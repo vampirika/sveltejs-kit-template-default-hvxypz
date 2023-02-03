11d08826a790ab28ee3d6765996356bcd4dda2ec
vampirika <59606542+vampirika@users.noreply.github.com>
2023-02-03 13:51:59 +0000

    1 file modified, 1 added

---
+++ src/routes/example/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<title>Example</title>
+	<meta name="description" content="Example page" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Example of me learning SvelteKit</h1>
+
+</div>
